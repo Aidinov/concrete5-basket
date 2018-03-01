e5ff0305250ff0ba9029107df24ee5c42f0b260e
Aidinov <gabosss0@gmail.com>
2018-03-01 18:45:19 +0500

    fix

--- src/Storage/Session.php
+++ src/Storage/Session.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * PHP version 7 package to handle your shopping basket for concrete5.
+ *
+ * @author    Andrei Aidinov <aidinov.andrey@gmail.com>
+ * @copyright 2018 Aidinov
+ * @license   Apache 2.0
+ * @version   GIT: <git_id> In development.
+ * @link      https://github.com/Aidinov/concrete5-basket
+ */
 namespace Aidinov\Concrete\Basket\Storage;
 
 use Aidinov\Basket\Storage\Runtime;
